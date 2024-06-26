a71ab0eeca8d3b6def7de8e99e7fed915ebe6078
rix <rix@localhost.localdomain>
2024-06-26 18:13:15 -0400

    update  the angular  project

--- Employee_Management_System_api/Migrations/20240626165529_InitialCreate.cs
+++ Employee_Management_System_api/Migrations/20240626165529_InitialCreate.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Employee_Management_System_api.Migrations
 {
-    /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
@@ -29,7 +27,6 @@ namespace Employee_Management_System_api.Migrations
                 });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
